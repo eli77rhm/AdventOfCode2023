c56c4c90f32abd2ac74a2b75f02a0057ea567ed8
Elham Rahimi <elham.rahimi@thetrainline.com>
2023-12-07 09:26:57 +0000

    Day6

---
+++ Aoc23/Day6/Day6.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aoc23
+{
+    public class Day6 : AllDays
+    {
+        public Day6() : base("Day6")
+        {
+        }
+
+        public override void ExecutePart1()
+        {
+            var raceTimes = Lines[0].Substring(5).Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            var recordDistances = Lines[1].Substring(9).Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            long totalWays = 1;
+            for (int i = 0; i < raceTimes.Length; i++)
+            {
+                int waysToWin = CalculateWaysToWin(raceTimes[i], recordDistances[i]);
+                totalWays *= waysToWin;
+            }
+
+            Console.WriteLine($"Total ways : {totalWays}");
+
+        }
+
+        private int CalculateWaysToWin(int raceTime, int recordDistance)
+        {
+            int ways = 0;
+            for (int holdTime = 0; holdTime < raceTime; holdTime++)
+            {
+                int speed = holdTime;
+                int travelTime = raceTime - holdTime;
+                int distance = speed * travelTime;
+
+                if (distance > recordDistance)
+                {
+                    ways++;
+                }
+            }
+            return ways;
+        }
+
+        public override void ExecutePart2()
+        {
+            long raceTime = long.Parse(Lines[0].Substring(5).Replace(" ", ""));
+            long recordDistance = long.Parse(Lines[1].Substring(9).Replace(" ", ""));
+
+            long waysToWin = CalculateWaysToWinPart2(raceTime, recordDistance);
+
+            Console.WriteLine($"Total ways : {waysToWin}");
+
+        }
+
+        private long CalculateWaysToWinPart2(long raceTime, long recordDistance)
+        {
+            long ways = 0;
+            for (long holdTime = 0; holdTime < raceTime; holdTime++)
+            {
+                long speed = holdTime;
+                long travelTime = raceTime - holdTime;
+                long distance = speed * travelTime;
+
+                if (distance > recordDistance)
+                {
+                    ways++;
+                }
+            }
+            return ways;
+        }
+    }
+}
--- Aoc23/Program.cs
+++ Aoc23/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            var day = new Day5();
+            var day = new Day6();
             day.ExecutePart2();
         }
     }
